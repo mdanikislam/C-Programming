e43286ce6519fac37363cc9b45ebef5c085531df
mdanikislam <anikislam487@gmail.com>
2022-01-27 23:24:41 +0600

    asciiTochar value

--- 6-size.c
+++ 6-size.c
@@ -6,9 +6,9 @@ int main(){
     double d;
     char c; 
     // here we use sizeof operator for get the size of keywords ?
-    printf("The size of integer is - %d\n", sizeof(i));  // int    = 4 byets
-    printf("The size of float is - %d\n", sizeof(f));    // float  = 4 bytes
-    printf("The size of double is - %d\n", sizeof(d));   // double = 8 bytes
-    printf("The size of character is - %d", sizeof(c));  // char   = 1 bytes
+    printf("The size of float is - %d bytes\n", sizeof(f));    // float  = 4 bytes
+    printf("The size of double is - %d bytes\n", sizeof(d));   // double = 8 bytes
+    printf("The size of integer is - %d bytes\n", sizeof(i));  // int    = 4 byets
+    printf("The size of character is - %d bytes", sizeof(c));  // char   = 1 bytes
 
 };
---
+++ 7.ASCII-to-char.c
@@ -0,0 +1,9 @@
+// c program to find a character of ASCII value 
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter any ASCII value : ");
+    scanf("%d", &n);
+    //  %c specifier for character value
+    printf("The ASCII Character value is = %c",n);
+}
