497e585d992715708f5235d91a91614d4a409775
mdanikislam <anikislam487@gmail.com>
2022-01-27 19:40:30 +0600

    sizeof keywords

--- 6-size.c
+++ 6-size.c
@@ -1,5 +1,14 @@
 // c program to find the size of int, float, double, char int=4 bytes, float=4 bytes, double=8 bytes, char=1 bytes
 #include<stdio.h>
 int main(){
+    int i;
+    float f;
+    double d;
+    char c; 
+    // here we use sizeof operator for get the size of keywords ?
+    printf("The size of integer is - %d\n", sizeof(i));  // int    = 4 byets
+    printf("The size of float is - %d\n", sizeof(f));    // float  = 4 bytes
+    printf("The size of double is - %d\n", sizeof(d));   // double = 8 bytes
+    printf("The size of character is - %d", sizeof(c));  // char   = 1 bytes
 
 };
